1bbc50f2023400275344f047d23cf416e978cb9c
Emre <cloudronek.z3k8w@aleeas.com>
2022-11-16 13:37:03 -0800

    Add Readme, update CRUD tests

---
+++ readme.md
@@ -0,0 +1,15 @@
+## .env file
+The environment file here is included as this is a challenge, not production-targeted work. In a normal situation, I would not include the `.env` file (already is in the `.gitignore`)
+
+## Test Cases for the UI
+1.
+2.
+3.
+4.
+5
+
+## Test Cases for the CRUD
+1.
+2.
+3.
+4.
--- tests/integration/crud.spec.ts
+++ tests/integration/crud.spec.ts
@@ -3,10 +3,21 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+test.use(
+    {
+        extraHTTPHeaders: {
+            'Accept': 'application/json',
+            'Authorization': `token ${process.env.API_TOKEN}`,
+          },
+    }
+)
+
 const PETSTOREURL = `https://petstore.swagger.io/v2/pet`;
 const PETID = 942003;
 const PETNAME = `chookitya`;
+const UPDATEDPETNAME = `changedcookity`;
 
+// CREATE
 test('Should create a new pet.', async ({ request }) => {
     const newPet = await request.post(`${PETSTOREURL}/`, {
       data: {
@@ -29,13 +40,16 @@ test('Should create a new pet.', async ({ request }) => {
       }
     });
     expect(newPet.ok()).toBeTruthy();
-  
-    const pets = await request.get(`${PETSTOREURL}/pets`);
+  });
+
+  // READ
+  test('Should find a newly created test', async ({ request }) => {
+    const pets = await request.get(`${PETSTOREURL}/${PETID}`);
     expect(pets.ok()).toBeTruthy();
     expect(await pets.json()).toContainEqual(expect.objectContaining({
-        "id": 942003,
+        "id": PETID,
         "category": {
-          "id": PETID,
+          "id": 0,
           "name": "string"
         },
         "name": PETNAME,
@@ -50,4 +64,38 @@ test('Should create a new pet.', async ({ request }) => {
         ],
         "status": "available"
       }));
+  })
+
+  // UPDATE
+  test('Update the freshly created pet name record.', async ({ request }) => {
+    const updatedPet = await request.put(`${PETSTOREURL}/`, {
+      data: {
+        "id": PETID,
+        "category": {
+          "id": 0,
+          "name": "string"
+        },
+        "name": UPDATEDPETNAME,
+        "photoUrls": [
+          "string"
+        ],
+        "tags": [
+          {
+            "id": 0,
+            "name": "string"
+          }
+        ],
+        "status": "available"
+      }
+    });
+    expect(updatedPet.ok()).toBeTruthy();
   });
+
+  // DELETE
+  test('Should retrieve the updated pet name', async ({ request }) => {
+    const deletePet = await request.delete(`${PETSTOREURL}/${PETID}`);
+    expect(deletePet.ok()).toBeTruthy();
+    expect(await deletePet.json()).toContainEqual(expect.objectContaining({
+    //  TODO: Implement the expected response/result from Pet Store Swagger API
+      }));
+  })
